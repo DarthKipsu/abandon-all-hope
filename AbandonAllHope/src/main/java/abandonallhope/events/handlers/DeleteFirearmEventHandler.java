f3e65e7db536b1a9f38cf01e8ca9af824fb4b529
Verna Koskinen <darth.kipsu@gmail.com>
2014-12-05 14:48:36 +0200

    improve tests for resource events

--- AbandonAllHope/src/main/java/abandonallhope/events/handlers/DeleteFirearmEventHandler.java
+++ AbandonAllHope/src/main/java/abandonallhope/events/handlers/DeleteFirearmEventHandler.java
@@ -7,6 +7,6 @@ import abandonallhope.events.action.DeleteFirearmEvent;
  * Handles deleting firearms from ui drop down lists.
  * @author kipsu
  */
-public interface DeleteFirearmEventHandler {
+public interface DeleteFirearmEventHandler extends ResourceEventHandler {
 	void handle(DeleteFirearmEvent e);
 }
--- AbandonAllHope/src/test/java/abandonallhope/logic/ResourceEventsTest.java
+++ AbandonAllHope/src/test/java/abandonallhope/logic/ResourceEventsTest.java
@@ -1,10 +1,21 @@
 package abandonallhope.logic;
 
+import abandonallhope.domain.Inventory;
 import abandonallhope.domain.Survivor;
+import abandonallhope.domain.weapons.Pistol;
+import abandonallhope.domain.weapons.Weapon;
+import abandonallhope.events.action.DeleteFirearmEvent;
 import abandonallhope.events.action.DeleteSurvivorEvent;
+import abandonallhope.events.action.DeleteWeaponEvent;
+import abandonallhope.events.action.NewFirearmEvent;
 import abandonallhope.events.action.NewSurvivorEvent;
+import abandonallhope.events.action.NewWeaponEvent;
+import abandonallhope.events.handlers.DeleteFirearmEventHandler;
 import abandonallhope.events.handlers.DeleteSurvivorEventHandler;
+import abandonallhope.events.handlers.DeleteWeaponEventHandler;
+import abandonallhope.events.handlers.NewFirearmEventHandler;
 import abandonallhope.events.handlers.NewSurvivorEventHandler;
+import abandonallhope.events.handlers.NewWeaponEventHandler;
 import abandonallhope.events.handlers.ResourceEventHandler;
 import org.junit.Before;
 import org.junit.Test;
@@ -23,6 +34,10 @@ public class ResourceEventsTest {
 	public void noEventHandlersInTheBeginning() {
 		assertTrue(resEvents.newSurvivorEventHandlers.isEmpty());
 		assertTrue(resEvents.deleteSurvivorEventHandlers.isEmpty());
+		assertTrue(resEvents.newFirearmEventHandlers.isEmpty());
+		assertTrue(resEvents.deleteFirearmEventHandlers.isEmpty());
+		assertTrue(resEvents.newWeaponEventHandlers.isEmpty());
+		assertTrue(resEvents.deleteWeaponEventHandlers.isEmpty());
 	}
 
 	@Test
@@ -37,6 +52,30 @@ public class ResourceEventsTest {
 		resEvents.addNewResourceEventHandler(deleteSurvivorEvent(), "deleteSurvivor");
 		assertEquals(1, resEvents.deleteSurvivorEventHandlers.size());
 		assertTrue(resEvents.newSurvivorEventHandlers.isEmpty());
+	}
+
+	@Test
+	public void newWeaponEventsCanBeAdded() {
+		resEvents.addNewResourceEventHandler(newWeaponEvent(), "newWeapon");
+		assertEquals(1, resEvents.newWeaponEventHandlers.size());
+	}
+
+	@Test
+	public void deleteWeaponEventsCanBeAdded() {
+		resEvents.addNewResourceEventHandler(deleteWeaponEvent(), "deleteWeapon");
+		assertEquals(1, resEvents.deleteWeaponEventHandlers.size());
+	}
+
+	@Test
+	public void newFirearmEventsCanBeAdded() {
+		resEvents.addNewResourceEventHandler(newFirearmEvent(), "newFirearm");
+		assertEquals(1, resEvents.newFirearmEventHandlers.size());
+	}
+
+	@Test
+	public void deleteFirearmEventsCanBeAdded() {
+		resEvents.addNewResourceEventHandler(deleteFirearmEvent(), "deleteFirearm");
+		assertEquals(1, resEvents.deleteFirearmEventHandlers.size());
 	}
 	
 	@Test
@@ -54,13 +93,63 @@ public class ResourceEventsTest {
 		resEvents.triggerDeleteSurvivorEvent(new Survivor(null, null, null, 1));
 		assertEquals(1, res.handleEvents);
 	}
+	
+	@Test
+	public void deleteFirearmEventsCanBeTriggered() {
+		DeleteFirearmEventHandlerMock res = (DeleteFirearmEventHandlerMock) deleteFirearmEvent();
+		resEvents.addNewResourceEventHandler(res, "deleteFirearm");
+		resEvents.triggerDeleteFirearmEvent(new Pistol(new Inventory()));
+		assertEquals(1, res.handleEvents);
+	}
+	
+	@Test
+	public void deleteWeaponEventsCanBeTriggered() {
+		DeleteWeaponEventHandlerMock res = (DeleteWeaponEventHandlerMock) deleteWeaponEvent();
+		resEvents.addNewResourceEventHandler(res, "deleteWeapon");
+		resEvents.triggerDeleteWeaponEvent(new Weapon(1, 1));
+		assertEquals(1, res.handleEvents);
+	}
 
 	private ResourceEventHandler newSurvivorEvent() {
 		return new NewSurvivorEventHandlerMock();
 	}
 
+	private ResourceEventHandler newFirearmEvent() {
+		return new NewFirearmEventHandlerMock();
+	}
+
+	private ResourceEventHandler newWeaponEvent() {
+		return new NewWeaponEventHandlerMock();
+	}
+
 	private ResourceEventHandler deleteSurvivorEvent() {
 		return new DeleteSurvivorEventHandlerMock();
+	}
+
+	private ResourceEventHandler deleteFirearmEvent() {
+		return new DeleteFirearmEventHandlerMock();
+	}
+
+	private ResourceEventHandler deleteWeaponEvent() {
+		return new DeleteWeaponEventHandlerMock();
+	}
+
+	private class NewWeaponEventHandlerMock implements NewWeaponEventHandler {
+		public int handleEvents = 0;
+		
+		@Override
+		public void handle(NewWeaponEvent e) {
+			handleEvents++;
+		}
+	}
+
+	private class NewFirearmEventHandlerMock implements NewFirearmEventHandler {
+		public int handleEvents = 0;
+		
+		@Override
+		public void handle(NewFirearmEvent e) {
+			handleEvents++;
+		}
 	}
 
 	private class NewSurvivorEventHandlerMock implements NewSurvivorEventHandler {
@@ -77,6 +166,24 @@ public class ResourceEventsTest {
 		
 		@Override
 		public void handle(DeleteSurvivorEvent e) {
+			handleEvents++;
+		}
+	}
+
+	private class DeleteWeaponEventHandlerMock implements DeleteWeaponEventHandler {
+		public int handleEvents = 0;
+		
+		@Override
+		public void handle(DeleteWeaponEvent e) {
+			handleEvents++;
+		}
+	}
+
+	private class DeleteFirearmEventHandlerMock implements DeleteFirearmEventHandler {
+		public int handleEvents = 0;
+		
+		@Override
+		public void handle(DeleteFirearmEvent e) {
 			handleEvents++;
 		}
 	}
